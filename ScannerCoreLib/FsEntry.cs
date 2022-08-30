406ab00b19439068ad5306f211c595b11265f04d
Эмин Назаров <nazarov.emin@outlook.com>
2022-08-30 18:22:03 +0300

    FsEntry added

---
+++ ScannerCoreLib/FsEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerCoreLib
+{
+    internal class FsEntry
+    {
+        public FsEntry(string name, long size, bool isDir, DateTime lastModified = default)
+        {
+            Name = name;
+            Size = size;
+            IsDir = isDir;
+            LastModified = lastModified == default ? DateTime.Now : lastModified;
+        }
+
+        public List<FsEntry> _children;
+
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public bool IsDir { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
